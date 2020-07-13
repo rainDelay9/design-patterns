ec07ffc6e0778636c2b48bd266ca4c68a5c1bd68
rainDelay9 <matan.orland@gmail.com>
2020-07-13 23:49:41 +0300

    Adding .gitignore

---
+++ strategy-pattern/cmake-build-debug/CMakeFiles/strategy_pattern.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/strategy_pattern.dir/main.cpp.o"
+  "strategy_pattern"
+  "strategy_pattern.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/strategy_pattern.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ strategy-pattern/cmake-build-debug/CMakeFiles/strategy_pattern.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for strategy_pattern.
+# This may be replaced when dependencies are built.
