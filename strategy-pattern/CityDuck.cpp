22a409b8e12614235cc5940e0111dbb99177545c
rainDelay9 <matan.orland@gmail.com>
2020-07-15 15:03:29 +0300

    Adding flying and quacking behaviors

--- strategy-pattern/CityDuck.cpp
+++
@@ -1,5 +0,0 @@
-//
-// Created by Matan on 7/15/20.
-//
-
-#include "CityDuck.h"
---
+++ strategy-pattern/CountryDuck.h
@@ -0,0 +1,19 @@
+//
+// Created by Matan on 7/15/20.
+//
+
+#ifndef STRATEGY_PATTERN_COUNTRYDUCK_H
+#define STRATEGY_PATTERN_COUNTRYDUCK_H
+
+#include "Duck.h"
+#include "SimpleQuackBehavior.h"
+#include "FreeFlyingBehavior.h"
+
+class CountryDuck : public Duck {
+public:
+    CountryDuck() : Duck(std::make_shared<SimpleQuackBehavior>(), std::make_shared<FreeFlyingBehavior>()) {
+
+    }
+};
+
+#endif //STRATEGY_PATTERN_COUNTRYDUCK_H
